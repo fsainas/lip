fd555809368999d7a0962d61f21a6d6f323b6eb4
Franco Sainas <franco@sainas.me>
2022-10-06 11:10:53 +0200

    better minfun

--- ocaml-basics/minfun/minfun_fsainas.ml
+++ ocaml-basics/minfun/minfun_fsainas.ml
@@ -1,13 +1,10 @@
 type 'a option = Some of 'a | None;;
 
-let rec minfun f a b = 
-    if a > b 
-    then failwith "a must be less or equal to b" 
-    else match (a,b) with 
+let rec minfun f a b = match (a,b) with 
         (* The range is empty *)
-          (a,b) when a = b -> None
-        (* b is the successor of a so the minimum is a or b *)
-        | (a,b) when (a+1) = b -> if (f a) <= (f b) then Some(a) else Some(b)
+          (a,b) when a > b -> None
+        (* There is only one element in the range *)
+        | (a,b) when a = b -> Some(a)
         (* narrow the range by removing the greatest element *)
         | (a,b) -> if (f a) < (f b) 
                     then minfun f a (b-1)
