9918f7007dc3ef80eb64ff35c4b9380223575baf
Franco Sainas <franco@sainas.me>
2022-10-06 13:01:21 +0200

    better string-of-list

--- ocaml-basics/string-of-list/string-of-list_fsainas.ml
+++ ocaml-basics/string-of-list/string-of-list_fsainas.ml
@@ -1,9 +1,9 @@
-let rec list_to_string = function      
-      [] -> "[]"
-    | h::[] -> string_of_int h
-    | h::t -> string_of_int h ^ ";" ^ list_to_string t
-;;
-let string_of_list l = "[" ^ list_to_string l ^ "]"
+let string_of_list l = 
+    let rec string_of_list_rec = function      
+          [] -> "[]"
+        | h::[] -> string_of_int h
+        | h::t -> string_of_int h ^ ";" ^ list_to_string t
+        in "[" ^ string_of_list_rec l ^ "]"
 ;;
 
 string_of_list [1;2;3;4;5];;
